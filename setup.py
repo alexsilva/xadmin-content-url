45d4efece08b4a301b0fe39a4417fc5a69365377
alexsilva <geniofuturo@gmail.com>
2023-06-20 10:19:17 -0300

    Add initial setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='xadmin-content-url',
+	version='1.0.0',
+	packages=[''],
+	url='https://github.com/alexsilva/xadmin-content-url',
+	license='MIT',
+	author='alex',
+	author_email='',
+	description=''
+)
