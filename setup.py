70b8d72210b4a3f7a92445c5aeb9e6d285238388
alexsilva <geniofuturo@gmail.com>
2023-06-20 18:53:39 -0300

    Package registration.

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
 	name='xadmin-content-url',
 	version='1.0.0',
-	packages=['xadmin_content_url'],
+	packages=[
+		'xadmin_content_url',
+		'xadmin_content_url.db',
+		'xadmin_content_url.forms'
+	],
 	url='https://github.com/alexsilva/xadmin-content-url',
 	license='MIT',
 	author='alex',
