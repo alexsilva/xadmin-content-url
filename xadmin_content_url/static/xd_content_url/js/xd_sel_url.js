b21a35cdad0b6d1ce00e0922216e36b4ca916f0f
alexsilva <geniofuturo@gmail.com>
2023-06-22 19:53:25 -0300

    Configuration of the selected data in the corresponding fields.

--- xadmin_content_url/static/xd_content_url/js/xd_sel_url.js
+++ xadmin_content_url/static/xd_content_url/js/xd_sel_url.js
@@ -1,15 +1,15 @@
 $(function () {
-    var Modal = function () {
-        this.$modal = $("#nunjucks-modal-main").template_render$({
-            modal: {size: 'modal-lg', id: "xd_content_url_modal"},
-            cancel_button: {'class': 'btn-sm'}
-        });
+    var Modal = function (options) {
+        this.$modal = $("#nunjucks-modal-main").template_render$(options);
     }
     Modal.prototype.loading = function () {
         return this.set_content('<h1 style="text-align:center;"><i class="fa-spinner fa-spin fa fa-large"></i></h1>');
     }
+    Modal.prototype.find = function (selector){
+        return this.$modal.find(selector)
+    }
     Modal.prototype.set_content = function (html) {
-        return this.$modal.find(".modal-body").html(html)
+        return this.find(".modal-body").html(html)
     }
 
     Modal.prototype.show = function () {
@@ -25,8 +25,9 @@ $(function () {
         $el.click($.proxy(this.init, this));
     }
     ContentUrl.prototype.init = function () {
-        var self= this,
+        var self = this,
             modal = this.get_modal();
+        this.get_btn_insert().prop("disabled", true);
         modal.loading();
         $.ajax({
             url: this.$el.data('url')
@@ -52,13 +53,25 @@ $(function () {
         }
     }
 
+    ContentUrl.prototype.get_btn_insert = function () {
+        if (!this.$btnInsert) {
+            this.$btnInsert = this.modal.find("button.xd_ct_insert");
+        }
+        return this.$btnInsert
+    }
+
+    ContentUrl.prototype.get_selection = function () {
+        return this.modal.find("form.xdm_ct_url_form #id_xdm-content").val();
+    }
+
     ContentUrl.prototype.ajax_table = function () {
-        var $form = this.modal.$modal.find("form.xdm_ct_url_form"),
+        var $form = this.modal.find("form.xdm_ct_url_form"),
             $sel = $form.find("#id_xdm-content"),
-            content = $sel.val(),
-            url = Urls["xadmin:" + content.replace(".", "_") + "_rest"](),
+            model_label = $sel.val(),
+            url = Urls["xadmin:" + model_label.replace(".", "_") + "_rest"](),
             $table = $form.find("table.xdm_ct_url_table").removeClass('d-none'),
             params = {plugin: "xd_ct_url", 'format': 'datatables'};
+
         if (!this.$dt) {
             this.$dt = $table.DataTable({
                 ajax: {
@@ -74,15 +87,48 @@ $(function () {
                     url: $table.data('language-url'),
                 },
             });
+            this.$dt.on('select', $.proxy(this.dt_row_selected, this));
+            this.$dt.on('deselect', $.proxy(this.dt_row_deselected, this));
         } else {
             this.$dt.ajax.url(url).load();
         }
     }
 
+    ContentUrl.prototype.dt_row_selected = function (e, dt, type, indexes) {
+        if (type === 'row') {
+            this.get_btn_insert().prop("disabled", false);
+        }
+    }
+    ContentUrl.prototype.dt_row_deselected = function (e, dt, type, indexes) {
+        if (type === 'row') {
+            this.get_btn_insert().prop("disabled", true);
+        }
+    }
+
+    ContentUrl.prototype.insert = function () {
+        var data = this.$dt.rows( { selected: true } ).data(),
+            data_id = data.pluck('id'),
+            data_url = data.pluck('url'),
+            model_label = this.get_selection(),
+            $input = $("form input[name='" + this.$el.data('for_name') +"']"),
+            $selInput = $("form input[name='sel_" + this.$el.data('for_name') +"']"),
+            ctype = model_label.replace(".", ":") + ":" + data_id[0];
+        $selInput.val(data_url[0])
+        $input.val(ctype);
+    }
+
     ContentUrl.prototype.get_modal = function () {
         if (!this.modal) {
-            this.modal = new Modal();
+            this.modal = new Modal({
+                modal: {size: 'modal-lg', id: "xd_content_url_modal"},
+                cancel_button: {
+                    'class': 'btn-sm xd_ct_insert',
+                    'text': gettext("Insert selected")
+                }
+            });
             this.modal.appendTo('body');
+            var $btn = this.get_btn_insert().prop("disabled", true);
+            $btn.click($.proxy(this.insert, this));
         }
         return this.modal;
     }
