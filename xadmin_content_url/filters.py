575bdd3f8ab3f202fb713dfc5285db6a2fa7ed5c
alexsilva <geniofuturo@gmail.com>
2023-06-22 20:47:08 -0300

    Fix search when empty value.

--- xadmin_content_url/filters.py
+++ xadmin_content_url/filters.py
@@ -3,9 +3,9 @@ from rest_framework.filters import BaseFilterBackend
 
 class SearchFilterBackend(BaseFilterBackend):
 	def filter_queryset(self, request, queryset, view):
+		search = request.query_params.get('search[value]')
 		search_fields = getattr(view, "xd_content_search_fields", None)
-		if search_fields:
-			search = request.query_params.get('search[value]')
+		if search_fields and search and (search := search.strip()):
 			search_fields = (search_fields
 			                 if isinstance(search_fields, (list, tuple)) else
 			                 (search_fields,))
