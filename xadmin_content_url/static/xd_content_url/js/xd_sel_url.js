34e2cdf014bb8e09b9f89912b1e094f20cef3937
alexsilva <geniofuturo@gmail.com>
2023-06-23 12:52:36 -0300

    Mounting the url with or without and lib 'Urls'

--- xadmin_content_url/static/xd_content_url/js/xd_sel_url.js
+++ xadmin_content_url/static/xd_content_url/js/xd_sel_url.js
@@ -64,11 +64,19 @@ $(function () {
         return this.modal.find("form.xdm_ct_url_form #id_xdm-content").val();
     }
 
+    ContentUrl.prototype.get_rest_url = function (model_label) {
+        var url;
+        if (window.Urls) {
+            url = Urls["xadmin:" + model_label.replace(".", "_") + "_rest"]()
+        } else {
+            url = xadmin.path_prefix + model_label.replace(".", "/") + "/rest"
+        }
+        return url;
+    }
     ContentUrl.prototype.ajax_table = function () {
         var $form = this.modal.find("form.xdm_ct_url_form"),
             $sel = $form.find("#id_xdm-content"),
-            model_label = $sel.val(),
-            url = Urls["xadmin:" + model_label.replace(".", "_") + "_rest"](),
+            url = this.get_rest_url($sel.val()),
             $table = $form.find("table.xdm_ct_url_table").removeClass('d-none'),
             params = {plugin: "xd_ct_url", 'format': 'datatables'};
 
