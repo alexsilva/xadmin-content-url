02b6ea202943baac9ecb0a8f747cb2f81f9abd6d
alexsilva <geniofuturo@gmail.com>
2024-01-19 12:48:24 -0300

    Escape title.

--- xadmin_content_url/rest/serializers/content.py
+++ xadmin_content_url/rest/serializers/content.py
@@ -1,14 +1,19 @@
 # coding=utf-8
 from rest_framework import serializers
+from django.utils import html
 
 
 class GenericContentUrlSerializer(serializers.ModelSerializer):
 	id = serializers.IntegerField(source="pk")
-	title = serializers.CharField(source='__str__')
+	title = serializers.SerializerMethodField('get_title')
 	url = serializers.SerializerMethodField("get_absolute_url", read_only=True)
 
 	def get_absolute_url(self, instance):
 		return instance.get_absolute_url()
+
+	def get_title(self, instance):
+		# handle xss
+		return html.escape(str(instance))
 
 	class Meta:
 		fields = ('id', 'title', 'url')
