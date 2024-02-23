01606ee0001ff095c1e6af75875a739f666e7c2c
alexsilva <geniofuturo@gmail.com>
2024-02-23 13:52:42 -0300

    Implementing 'has_changed' with validation of url data for comparison purposes.

--- xadmin_content_url/forms/fields.py
+++ xadmin_content_url/forms/fields.py
@@ -1,6 +1,7 @@
 import django.forms as django_forms
 from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
 from xadmin_content_url.forms import widgets
 from xadmin_content_url.models import XdUrl
 
@@ -19,6 +20,35 @@ class XdContentUrlField(django_forms.Field):
 			return value
 		else:
 			return self.to_python(value)
+
+	def has_changed(self, initial, data):
+		"""Return True if data differs from initial."""
+		# Always return False if the field is disabled since self.bound_data
+		# always uses the initial value in this case.
+		if self.disabled:
+			return False
+		try:
+			data = self.to_python(data)
+			if hasattr(self, '_coerce'):
+				return self._coerce(data) != self._coerce(initial)
+		except ValidationError:
+			return True
+		# For purposes of seeing whether something has changed, None is
+		# the same as an empty string, if the data or initial value we get
+		# is None, replace it with ''.
+		initial_values: list[XdUrl] = initial if initial is not None else ()
+		data_values: list[XdUrl] = data if data is not None else ()
+		changed = False
+		for data_val in data_values:
+			for initial_val in initial_values:
+				if (data_val.pk and data_val.pk == initial_val.pk or
+						data_val.content_type == initial_val.content_type and
+						data_val.object_id == initial_val.object_id):
+					break
+			else:
+				changed = True
+				break
+		return changed
 
 	def to_python(self, value):
 		if value in self.empty_values:
